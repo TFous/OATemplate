78b7c85776c9908c43ad5dce7a3dc846671e8262
Teng <475984203@qq.com>
2018-02-27 16:03:32 +0800

    edit

--- template/src/components/layout/components/Sidebar/SidebarItem/SidebarItem.ts
+++ template/src/components/layout/components/Sidebar/SidebarItem/SidebarItem.ts
@@ -16,8 +16,8 @@ export default class SidebarItem extends Vue {
     @Prop()
     routes: array
 
-    generateTitle (title="") {
-        return title // $t :this method from vue-i18n ,inject in @/lang/index.js
+    generateTitle (title) {
+        return title||'' // $t :this method from vue-i18n ,inject in @/lang/index.js
     }
 
 }
